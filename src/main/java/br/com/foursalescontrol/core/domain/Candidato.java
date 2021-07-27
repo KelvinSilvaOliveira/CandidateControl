3a39b4ebc4739784dd097d5218771d41d1841c63
KelvinSilvaOliveira <kelvinsilva1010@gmail.com>
2021-07-27 02:41:05 -0300

    feat: implementa validação nas classes de dominio

--- src/main/java/br/com/foursalescontrol/core/domain/Candidato.java
+++ src/main/java/br/com/foursalescontrol/core/domain/Candidato.java
@@ -3,6 +3,7 @@ package br.com.foursalescontrol.core.domain;
 import java.util.List;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
@@ -17,8 +18,15 @@ import lombok.NoArgsConstructor;
 public class Candidato {
 	
 	private Long id;
+	
+	@NotEmpty(message = "Nome deve ser informado.")
 	private String nome;
+	
+	@NotEmpty(message = "CPF deve ser informado.")
+	@Pattern(regexp = "[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}\\-[0-9]{2}", message = "CPF deve ser correto.")
 	private String cpf;
+	
+	@Null(message = "Cartões não devem ser informados.")
 	private List<Cartao> cartoes;
 	
 }
--- src/main/java/br/com/foursalescontrol/core/domain/Cartao.java
+++ src/main/java/br/com/foursalescontrol/core/domain/Cartao.java
@@ -2,6 +2,8 @@ package br.com.foursalescontrol.core.domain;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Null;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,17 @@ import lombok.NoArgsConstructor;
 public abstract class Cartao {
 	
 	private Long id;
+	
+	@Null(message = "Número do cartão não deve ser informado.")
 	private String numero;
+	
+	@Null(message = "Nome não deve ser informado.")
 	private String nomeImpresso;
+	
+	@Null(message = "CVV do cartão não deve ser informado.")
 	private Integer cvv;
+	
+	@Null(message = "Data de validade não deve ser informado.")
 	private LocalDate dataValidade;
 	
 }
